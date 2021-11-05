3c61788b424cfd69026e70320c75e437077150c9
vinaykumarsingh <vs3486@gmail.com>
2021-11-05 12:28:45 +0530

    parent child communication -2

--- src/app/components/componentComunication/parent-child/child/child.component.ts
+++ src/app/components/componentComunication/parent-child/child/child.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-child',
@@ -9,16 +11,30 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class Appchild implements OnInit {
   @Input() userNameInChild: any
   @Input() notifications: any[]
+  @Output() newItemEvent = new EventEmitter<string>();
+
 
   constructor() {
     console.log('inside constructure')
 
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if(changes.userNameInChild.previousValue !== changes.userNameInChild.currentValue) {
+      console.log('userNameInChild variable value has changed in parent')
+    }
+  }
+
   ngOnInit() {
     console.log('inside ngOnInit')
     console.log('inside ngOnInit', this.notifications)
     
+  }
+
+  addNewItem(valueInChild):void {
+    console.log('valueInChild', valueInChild)
+    this.newItemEvent.emit(valueInChild);
   }
 
 }
--- src/app/components/componentComunication/parent-child/parent/parent.component.ts
+++ src/app/components/componentComunication/parent-child/parent/parent.component.ts
@@ -8,13 +8,27 @@ import { Component } from '@angular/core';
 export class Appparent {
   userNameInParent = "vinay"
 
-  notifications = [
+  notifications111 = [
     { id: 1, message: 'This is the first notification' },
     { id: 2, message: 'This is the second notification' },
   ];
 
   constructor() {
     console.log('inside constructure')
+  }
+  
+  addItem(value) {
+    console.log('value coming from child is ->', value)
+    let tempObj = {
+      id: this.notifications111.length + 1,
+      message: value
+    }
+    this.notifications111.push(tempObj)
+  }
+
+  changeValueInParent(){
+    console.log('changeValueInParent is clicked');
+    this.userNameInParent = "vinay singh"
   }
 
 }
